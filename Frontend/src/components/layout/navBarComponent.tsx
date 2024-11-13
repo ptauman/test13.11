dc01f66d64e63ccabdb02cad01d6ab49a8c99f14
pinch <pinchasmt@gmail.com>
2024-11-13 17:56:00 +0200

    A function to return missiles (as an object) for a relevant area. Beginning of work on socket.

--- Frontend/src/components/layout/navBarComponent.tsx
+++ Frontend/src/components/layout/navBarComponent.tsx
@@ -10,12 +10,11 @@ export const NavBarComponent : React.FC = () => {
          <AppBar position="sticky">
             <Toolbar>
                 <Button color="inherit" component={Link} to="/">Home</Button>
-                <Button color="inherit" component={Link} to="/register">Reqgister</Button>
+                <Button color="inherit" component={Link} to="/register">Register</Button>
                 <Button color="inherit" component={Link} to="/login">Login</Button>
                 <Button color="inherit" component={Link} to="/options">Options</Button>
             </Toolbar>
         </AppBar>
         </>
     )
-    
 }
